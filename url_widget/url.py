9af50eaf74512ecd85c032810ac6a082206f58fe
Alex-Lewandowski <aflewandowski@alaska.edu>
2021-11-05 12:57:27 -0800

    correct front-end module version

--- url_widget/url.py
+++ url_widget/url.py
@@ -1,6 +1,8 @@
 import ipywidgets as widgets
 from traitlets import Unicode
 
+from _version import version
+
 # See js/lib/widget.js for the frontend counterpart to this file.
 
 @widgets.register
@@ -20,9 +22,9 @@ class URLWidget(widgets.DOMWidget):
     _model_module = Unicode('url-widget').tag(sync=True)
 
     # Version of the front-end module containing widget view
-    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _view_module_version = Unicode(version).tag(sync=True)
     # Version of the front-end module containing widget model
-    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode(version).tag(sync=True)
 
     # Widget specific property.
     # Widget properties are defined as traitlets. Any property tagged with `sync=True`
