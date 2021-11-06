455762e7ceaf32f06cd382eabf3ddfca9deb1556
Alex-Lewandowski <aflewandowski@alaska.edu>
2021-11-05 17:36:49 -0800

    cut __version__ import

--- url_widget/__init__.py
+++ url_widget/__init__.py
@@ -5,7 +5,6 @@
 # Distributed under the terms of the Modified BSD License.
 
 from .url import URLWidget
-from ._version import __version__, version_info
 
 def _jupyter_labextension_paths():
     """Called by Jupyter Lab Server to detect if it is a valid labextension and
