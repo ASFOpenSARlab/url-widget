1d4bd069dd9cbfadf8437612a56c7ab3fc944bd8
Alex-Lewandowski <aflewandowski@alaska.edu>
2021-11-08 11:39:55 -0900

    version

--- url_widget/_version.py
+++ url_widget/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Alex Lewandowski.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 0, 'dev')
+version_info = (0, 1, 7, 'dev0', 'gef04251.d20211108')
 __version__ = ".".join(map(str, version_info))
